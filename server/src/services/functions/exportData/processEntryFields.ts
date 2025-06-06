c0c4edd79ca071a4f8d753903dda1ccdd7bf093a
Phillip Austerfield <phillip.austerfield@idmedia.com>
2025-06-06 08:41:02 +0100

    refractor

--- server/src/services/functions/exportData/processEntryFields.ts
+++ server/src/services/functions/exportData/processEntryFields.ts
@@ -18,126 +18,163 @@ along with this program; if not, see https://www.gnu.org/licenses/old-licenses/g
 import jsonToHtml from './jsonToHtml';
 import processComponent from './processComponent';
 import isFieldLocalizable from './isFieldLocalizable';
+import { TranslationstudioTranslatable } from '../../../../../Types';
 
 const DEFAULT_FIELDS = new Set([
-  'id',
-  'documentId',
-  'createdAt',
-  'updatedAt',
-  'publishedAt',
-  'locale',
-  'localizations',
-  'updatedBy',
-  'createdBy',
+    'id',
+    'documentId',
+    'createdAt',
+    'updatedAt',
+    'publishedAt',
+    'locale',
+    'localizations',
+    'updatedBy',
+    'createdBy',
 ]);
 
+export interface ExportedDocumentFields {
+    fields: any[],
+    keep: any
+};
+
 const isEmpty = (value: any) =>
-  value === null || value === undefined || value === '';
+    value === null || value === undefined || value === '';
 
 const isTranslatableField = (fieldSchema: any) =>
-  ['string', 'text', 'blocks', 'richtext'].includes(fieldSchema.type) &&
-  fieldSchema.pluginOptions?.i18n?.localized !== false;
+    ['string', 'text', 'blocks', 'richtext'].includes(fieldSchema.type) &&
+    fieldSchema.pluginOptions?.i18n?.localized !== false;
 
 const processDynamicZone = async (key: string, value: any[], schema: any) => {
-  const results = [];
+    const results = [];
 
-  for (const component of value) {
-    const componentName = component?.__component;
-    if (!componentName) continue;
+    for (const component of value) {
+        const componentName = component?.__component;
+        if (!componentName) continue;
 
-    const fields = await processComponent(
-      key,
-      componentName,
-      component,
-      componentName,
-      component.id
-    );
-    results.push(...fields);
-  }
+        const fields = await processComponent(
+            key,
+            componentName,
+            component,
+            componentName,
+            component.id
+        );
+        results.push(...fields);
+    }
 
-  return results;
+    return results;
 };
 
 const processComponentField = async (key: string, value: any, fieldSchema: any) => {
-  const results = [];
+    const results = [];
 
-  if (fieldSchema.repeatable && Array.isArray(value)) {
-    for (const component of value) {
-      const fields = await processComponent(
-        key,
-        fieldSchema.component,
-        component,
-        fieldSchema.component,
-        component.id
-      );
-      results.push(...fields);
+    if (fieldSchema.repeatable && Array.isArray(value)) {
+        for (const component of value) {
+            const fields = await processComponent(
+                key,
+                fieldSchema.component,
+                component,
+                fieldSchema.component,
+                component.id
+            );
+            results.push(...fields);
+        }
+    } else {
+        const fields = await processComponent(
+            key,
+            fieldSchema.component,
+            value,
+            fieldSchema.component,
+            value.id
+        );
+        results.push(...fields);
     }
-  } else {
-    const fields = await processComponent(
-      key,
-      fieldSchema.component,
-      value,
-      fieldSchema.component,
-      value.id
-    );
-    results.push(...fields);
-  }
 
-  return results;
+    return results;
 };
 
-const processRegularField = (key: string, value: any, fieldSchema: any) => {
-  const translatedValue =
-    fieldSchema.type === 'blocks' ? jsonToHtml(value) : value.toString();
+const processRegularField = (key: string, value: any, fieldSchema: any):TranslationstudioTranslatable => {
+    const translatedValue =
+        fieldSchema.type === 'blocks' ? jsonToHtml(value) : value.toString();
 
-  return {
-    field: key,
-    type: ['richtext', 'blocks'].includes(fieldSchema.type) ? 'html' : 'text',
-    translatableValue: [translatedValue],
-    realType: fieldSchema.type,
-  };
+    return {
+        field: key,
+        type: ['richtext', 'blocks'].includes(fieldSchema.type) ? 'html' : 'text',
+        translatableValue: [translatedValue],
+        realType: fieldSchema.type,
+    };
 };
 
-const processEntryFields = async (entry: any, schema: any, locale: string) => {
-  const contentFields = [];
+const processEntryFields = async (entry: any, schema: any, locale: string): Promise<ExportedDocumentFields> => {
+    const contentFields = [];
+    const staticContent:any = { };
+    
+    let randomIdNumber = 0;
 
-  for (const [key, value] of Object.entries(entry)) {
-    if (shouldSkipField(key, value)) continue;
+    for (const [key, value] of Object.entries(entry)) 
+    {
+        randomIdNumber++;
+        
+        if (shouldSkipField(key, value)) continue;
 
-    const fieldSchema = schema[key];
-    if (!fieldSchema) continue;
+        const fieldSchema = schema[key];
+        if (!fieldSchema) continue;
 
-    if (isDynamicZone(fieldSchema, value, schema)) {
-      const zoneFields = await processDynamicZone(key, value as any[], schema);
-      contentFields.push(...zoneFields);
-      continue;
+        /** ignore everything that is non-localiaable by default */
+        if (!isFieldLocalizable(fieldSchema, schema))
+            continue;
+
+        if (isTranslatableField(fieldSchema)) {
+            const translatedField = processRegularField(key, value, fieldSchema);
+            contentFields.push(translatedField);
+            staticContent[key] = value;
+            continue;
+        }
+
+        {
+            const zoneInfo = isDynamicZone(fieldSchema, value);
+            if (zoneInfo.isZone) {
+                if (zoneInfo.hasContent) {
+                    const zoneFields = await processDynamicZone(key, value as any[], schema);
+                    contentFields.push(...zoneFields);
+                    staticContent[key] = value;
+                }
+
+                continue;
+            }
+        }
+
+        const componentInfo = isComponent(fieldSchema);
+        if (componentInfo.isZone) {
+            const componentFields = await processComponentField(key, value, fieldSchema);
+            contentFields.push(...componentFields);
+            staticContent[key] = value;
+        }
     }
 
-    if (isComponent(fieldSchema, value, schema)) {
-      const componentFields = await processComponentField(key, value, fieldSchema);
-      contentFields.push(...componentFields);
-      continue;
-    }
-
-    if (isTranslatableField(fieldSchema)) {
-      const translatedField = processRegularField(key, value, fieldSchema);
-      contentFields.push(translatedField);
-    }
-  }
-
-  return contentFields;
+    return {
+        fields: contentFields,
+        keep: staticContent
+    };
 };
 
 const shouldSkipField = (key: string, value: any): boolean => {
-  return DEFAULT_FIELDS.has(key) || isEmpty(value);
+    return DEFAULT_FIELDS.has(key) || isEmpty(value);
 };
 
-const isDynamicZone = (fieldSchema: any, value: any, schema: any): boolean => {
-  return fieldSchema.type === 'dynamiczone' && isFieldLocalizable(fieldSchema, schema) && Array.isArray(value);
+const isDynamicZone = (fieldSchema: any, value: any) => {
+    return {
+        isZone: fieldSchema.type === 'dynamiczone',
+        hasContent: Array.isArray(value) && value.length > 0,
+    }
 };
 
-const isComponent = (fieldSchema: any, value: any, schema: any): boolean => {
-  return fieldSchema.type === 'component' && isFieldLocalizable(fieldSchema, schema);
+const isComponent = (fieldSchema: any) => {
+    return {
+        isZone: fieldSchema.type === 'dynamiczone',
+        hasContent: true
+    }
 };
+
+
 
 export default processEntryFields;
